c3b6144ee0f2d5066954fdc41fe4f228642768a7
JK-le-dev <jkcondor13@gmail.com>
2024-12-15 16:05:08 -0600

    fix(testing): fixed jest errors
    Jest required some mock ups the carousel portion of the app. Now tests
    are successful and yeah.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,72 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// Mock the IntersectionObserver, see https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+export class IntersectionObserver {
+  root = null;
+  rootMargin = "";
+  thresholds = [];
+
+  disconnect() {
+    return null;
+  }
+
+  observe() {
+    return null;
+  }
+
+  takeRecords() {
+    return [];
+  }
+
+  unobserve() {
+    return null;
+  }
+}
+export class ResizeObserver {
+  root = null;
+  rootMargin = "";
+  thresholds = [];
+
+  disconnect() {
+    return null;
+  }
+
+  observe() {
+    return null;
+  }
+
+  takeRecords() {
+    return [];
+  }
+
+  unobserve() {
+    return null;
+  }
+}
+window.IntersectionObserver = IntersectionObserver;
+global.IntersectionObserver = IntersectionObserver;
+window.ResizeObserver = ResizeObserver;
+global.ResizeObserver = ResizeObserver;
+
+describe('Test', () => {
+  // mock window.matchMedia, see https://stackoverflow.com/questions/39830580/jest-test-fails-typeerror-window-matchmedia-is-not-a-function
+  beforeAll(() => {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // Deprecated
+        removeListener: jest.fn(), // Deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
+  });
+  test('renders app', () => {
+    render(<App />);
+  });
 });
