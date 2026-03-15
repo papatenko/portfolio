f6fcd80f7d8236a5fa414ab55326115bcbb2d317
papatenko <justinkondratenko@proton.me>
2026-03-15 10:59:40 -0500

    header changed now let me go

--- src/components/sections/header.js
+++ src/components/sections/header.js
@@ -1,4 +1,5 @@
 import ThemeToggle from "components/ui/theme-toggle";
+import { Github } from "lucide-react";
 import { motion } from "motion/react";
 
 function Header() {
@@ -14,9 +15,19 @@ function Header() {
         ease: [0, 0.7, 0.2, 1],
       }}
     >
-      <div className="hidden md:flex items-center text-4xl text-primary font-bold">
-        Justin Kondratenko
+      <div className="hidden md:flex items-center text-4xl text-primary font-bold bg-secondary/50 rounded-full p-2 pr-4 box-shadow border-solid border-2 border-secondary/10 backdrop-blur-xl">
+        <motion.div
+          className="bg-primary p-2 rounded-full mr-2 text-background"
+          initial={{ opacity: 1 }}
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95 }}
+          transition={{ duration: 0.1 }}
+        >
+          <a href="https://github.com/papatenko">{<Github size={32} />}</a>
+        </motion.div>{" "}
+        Papatenko
       </div>
+
       <div className="flex gap-2 md:gap-4 items-center text-xl md:text-2xl text-background font-bold bg-secondary/50 rounded-full p-2 box-shadow border-solid border-2 border-secondary/10 backdrop-blur-xl">
         <Hyperlink href="#projects">Projects</Hyperlink>
         <Hyperlink href="#artwork">Artwork</Hyperlink>
